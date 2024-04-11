44bc428f7df6e3abac5c6b681128b26cb64b9979
bach0128 <125292149+bach0128@users.noreply.github.com>
2024-04-11 11:38:51 +0700

    up

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,6 @@ export default async function RootLayout({ children }) {
             <Footer />
           </SessionProvider>
         </Providers>
-
         <ToastContainer />
       </body>
     </html>
